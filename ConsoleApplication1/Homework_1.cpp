f16f72b133b9f55443380cfd172b65481b63690a
Ilya Ivanov <malycheharovaleksey@gmail.com>
2022-05-23 15:29:46 +0900

    second commit

---
+++ ConsoleApplication1/Homework_1.cpp
@@ -0,0 +1,12 @@
+﻿
+//#include <iostream>
+
+//int main()
+//{
+  //  setlocale(LC_ALL, "");
+    //std::cout << "First string\n";
+    //std::cout << "Первая строка\n";
+    //std::cout << "Вторая строка\n";
+    //std::cout <<"\"Спецсимволы\"\n";
+    //std::cout << "\/Спецсимволы/\n";
+//}
